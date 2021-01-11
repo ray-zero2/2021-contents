5b6f77a4daef485604e2f8c54d62a60d84ca1872
Rei Matsuda <rei2219.matsuda@gmail.com>
2021-01-11 23:42:43 +0900

    update webpack settings

--- resources/webpack.config.js
+++ resources/webpack.config.js
@@ -1,68 +1,89 @@
-// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
 const path = require('path');
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
-  mode: 'production',
-  // エントリーポイントの設定
-  entry: './js/index.js',
-  // 出力の設定
-  output: {
-    // 出力するファイル名
-    filename: '[name].bundle.js',
-    // 出力先のパス（絶対パスを指定する必要がある）
-    path: path.join(__dirname, '../public/js'),
-  },
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
 
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          // node_modules配下のモジュールをバンドル対象とする
-          test: /node_modules/,
-          name: 'vendor',
-          chunks: 'initial',
-          enforce: true,
+  return {
+    mode: 'production',
+
+    entry: './js/index.js',
+
+    devtool: isDevelopment ? 'eval-cheap-module-source-map' : false,
+
+    cache: {
+      type: 'filesystem',
+      buildDependencies: {
+        config: [__filename],
+      },
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.join(__dirname, '../public/js'),
+    },
+
+    optimization: {
+      minimize: !isDevelopment,
+      minimizer: isDevelopment
+        ? []
+        : [
+            new TerserPlugin({
+              terserOptions: {
+                compress: { drop_console: true },
+              },
+            }),
+          ],
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /node_modules/,
+            name: 'vendor',
+            chunks: 'all',
+            enforce: true,
+          },
         },
       },
     },
-  },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    modules: false,
-                    targets: {
-                      browsers: ['> 1%', 'last 3 versions', 'ie 11'],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  [
+                    '@babel/preset-env',
+                    {
+                      modules: false,
+                      targets: {
+                        browsers: ['> 1%', 'last 3 versions', 'ie 11'],
+                      },
+                      debug: true,
+                      useBuiltIns: 'usage',
+                      corejs: 3,
                     },
-                    debug: true,
-                    useBuiltIns: 'usage',
-                    corejs: 3,
-                  },
+                  ],
                 ],
-              ],
+              },
             },
-          },
-        ],
-      },
-      {
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        exclude: /node_modules/,
-        use: ['raw-loader', 'glslify-loader'],
-      },
-    ],
-  },
-  devtool: 'source-map',
-  plugins: [],
+          ],
+        },
+        {
+          test: /\.(glsl|vs|fs|vert|frag)$/,
+          exclude: /node_modules/,
+          use: ['raw-loader', 'glslify-loader'],
+          type: 'javascript/auto',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.glsl', '.vert', '.frag', '.vs', '.fs'],
+    },
+    target: ['web', 'es5'],
+  };
 };
